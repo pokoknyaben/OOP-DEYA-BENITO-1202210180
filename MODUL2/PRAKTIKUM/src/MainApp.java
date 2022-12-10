130cd686f869f2fce7905ea6eb7135d1e3aa0b89
pokoknyaben <85600785+pokoknyaben@users.noreply.github.com>
2022-12-10 07:46:06 +0700

    BENITO_1202210180

--- MODUL2/PRAKTIKUM/src/MainApp.java
+++ MODUL2/PRAKTIKUM/src/MainApp.java
@@ -1,20 +1,20 @@
 public class MainApp {
     public static void main(String[] args) throws Exception {
-        TransportasiAir ta1 = new TransportasiAir(6, 10000);
-        ta1.informasi();
-        ta1.berlayar();
-        ta1.berlabuh();
+        TransportasiAir transportAir = new TransportasiAir(6, 10000);
+        transportAir.informasi();
+        transportAir.berlayar();
+        transportAir.berlabuh();
         System.out.println("");
-        Sampan sampan1 = new Sampan(15000, 20, 1);
-        sampan1.informasi();
-        sampan1.berlayar();
-        sampan1.berlabuh();
-        sampan1.berlabuh(1);
+        Sampan sampanAja = new Sampan(15000, 20, 1);
+        sampanAja.informasi();
+        sampanAja.berlayar();
+        sampanAja.berlabuh();
+        sampanAja.berlabuh(1);
         System.out.println("");
-        Kapal kapal1 = new Kapal(400, 80000, "Diesel");
-        kapal1.informasi();
-        kapal1.berlayar();
-        kapal1.berlayar(18);
-        kapal1.berlabuh();
+        Kapal kapalAja = new Kapal(400, 80000, "Diesel");
+        kapalAja.informasi();
+        kapalAja.berlayar();
+        kapalAja.berlayar(18);
+        kapalAja.berlabuh();
     }
 }
